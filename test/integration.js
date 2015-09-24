aa8b969b3fb7a9102a7124594e81df296f5de3f5
Rolf Timmermans <rolftimmermans@voormedia.com>
2015-09-24 17:12:08 +0200

    Run integration tests.

---
+++ test/integration.js
@@ -0,0 +1,37 @@
+"use strict"
+
+if (process.env.TRAVIS_PULL_REQUEST && process.env.TRAVIS_PULL_REQUEST !== "false") process.exit()
+
+var tinify = require("../lib/tinify")
+var assert = require("chai").assert
+var tmp = require("tmp")
+var fs = require("fs")
+
+describe("client", function() {
+  var optimized
+
+  before(function() {
+    if (!process.env.TINIFY_KEY) {
+      console.log("Set the TINIFY_KEY environment variable.")
+      process.exit(1)
+    }
+    tinify.key = process.env.TINIFY_KEY
+
+    var unoptimizedPath = __dirname + "/examples/voormedia.png"
+    optimized = tinify.fromFile(unoptimizedPath)
+  })
+
+  it("should compress", function() {
+    var file = tmp.fileSync()
+    return optimized.toFile(file.name).then(function() {
+      assert.isBelow(fs.statSync(file.name).size, 1500)
+    })
+  })
+
+  it("should resize", function() {
+    var file = tmp.fileSync()
+    return optimized.resize({ method: "fit", width: 50, height: 20 }).toFile(file.name).then(function() {
+      assert.isBelow(fs.statSync(file.name).size, 800)
+    })
+  })
+})
