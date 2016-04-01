e3cb659243ec3f919d1dc029d92a128159d30dc7
Rolf Timmermans <rolftimmermans@voormedia.com>
2016-04-01 10:35:49 +0200

    Add integration test for metadata preservation.

--- test/integration.js
+++ test/integration.js
@@ -25,8 +25,15 @@ describe("client integration", function() {
   it("should compress from file", function() {
     var file = tmp.fileSync()
     return optimized.toFile(file.name).then(function() {
-      assert.isAbove(fs.statSync(file.name).size, 0)
-      assert.isBelow(fs.statSync(file.name).size, 1500)
+      var size = fs.statSync(file.name).size
+      var contents = fs.readFileSync(file.name).toString("hex")
+
+      assert.isAbove(size, 1000)
+      assert.isBelow(size, 1500)
+
+      /* width == 137 */
+      assert.include(contents, new Buffer([0, 0, 0, 0x89]).toString("hex"))
+      assert.notInclude(contents, new Buffer("Copyright Voormedia").toString("hex"))
     })
   })
 
@@ -34,16 +41,45 @@ describe("client integration", function() {
     var source = tinify.fromUrl("https://raw.githubusercontent.com/tinify/tinify-nodejs/master/test/examples/voormedia.png")
     var file = tmp.fileSync()
     return source.toFile(file.name).then(function() {
-      assert.isAbove(fs.statSync(file.name).size, 0)
-      assert.isBelow(fs.statSync(file.name).size, 1500)
+      var size = fs.statSync(file.name).size
+      var contents = fs.readFileSync(file.name).toString("hex")
+
+      assert.isAbove(size, 1000)
+      assert.isBelow(size, 1500)
+
+      /* width == 137 */
+      assert.include(contents, new Buffer([0, 0, 0, 0x89]).toString("hex"))
+      assert.notInclude(contents, new Buffer("Copyright Voormedia").toString("hex"))
     })
   })
 
   it("should resize", function() {
     var file = tmp.fileSync()
     return optimized.resize({ method: "fit", width: 50, height: 20 }).toFile(file.name).then(function() {
-      assert.isAbove(fs.statSync(file.name).size, 0)
-      assert.isBelow(fs.statSync(file.name).size, 1000)
+      var size = fs.statSync(file.name).size
+      var contents = fs.readFileSync(file.name).toString("hex")
+
+      assert.isAbove(size, 500)
+      assert.isBelow(size, 1000)
+
+      /* width == 50 */
+      assert.include(contents, new Buffer([0, 0, 0, 0x32]).toString("hex"))
+      assert.notInclude(contents, new Buffer("Copyright Voormedia").toString("hex"))
+    })
+  })
+
+  it("should preserve metadata", function() {
+    var file = tmp.fileSync()
+    return optimized.preserve("copyright", "creation").toFile(file.name).then(function() {
+      var size = fs.statSync(file.name).size
+      var contents = fs.readFileSync(file.name).toString("hex")
+
+      assert.isAbove(size, 1000)
+      assert.isBelow(size, 2000)
+
+      /* width == 137 */
+      assert.include(contents, new Buffer([0, 0, 0, 0x89]).toString("hex"))
+      assert.include(contents, new Buffer("Copyright Voormedia").toString("hex"))
     })
   })
 })
